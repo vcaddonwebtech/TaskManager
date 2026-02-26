fdb6351efa60e2c20343b8198ece431253557335
vcaddonwebtech <vcaddonwebtech@gmail.com>
2026-02-26 17:53:26 +0530

    delete button improved

--- src/features/taskSlice.js
+++ src/features/taskSlice.js
@@ -1,27 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const tasklSlice = createSlice({
-    name : "task",
-    initialState : {tasks: []},
-    reducers: {
-        addTask : (state,action)=>{
-            console.log("DataTask in the Reducers",action.payload)
-           state.tasks.push(action.payload)
-        },
-        showTask :(state ,action)=>{
-            state.tasks = action.payload;
-        },
-        updateTask :(state ,action)=>{
-            state.tasks.map((items ,index)=>(
-                items.id === action.payload.id ? action.payload : items
-            ));
-        },
-        deleteTask :(state ,action)=>{
-            state.tasks.filter(items=>items.id!=action.payload)
-        },
-    }
-})
+  name: "task",
+  initialState: { tasks: [] },
+  reducers: {
+    addTask: (state, action) => {
+      console.log("DataTask in the Reducers", action.payload);
+      state.tasks.push(action.payload);
+    },
+    showTask: (state, action) => {
+      state.tasks = action.payload;
+    },
+    updateTask: (state, action) => {
+      state.tasks = state.tasks.map((items, index) =>
+        items.id === action.payload.id ? action.payload : items,
+      );
+    },
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter((items) => items.id != action.payload);
+      console.log("After Deletation : ", state.tasks);
+    },
+  },
+});
 
-
-export const {addTask ,showTask,updateTask,deleteTask} = tasklSlice.actions
-export default tasklSlice.reducer 
+export const { addTask, showTask, updateTask, deleteTask } = tasklSlice.actions;
+export default tasklSlice.reducer;
